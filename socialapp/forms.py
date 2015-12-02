f477215934c9bccf5b06224fe33e70b657273f79
Iulia Chiriac <iulia.chiriac@eaudeweb.ro>
2015-12-03 00:23:37 +0200

    Some basic styling.

--- socialapp/forms.py
+++ socialapp/forms.py
@@ -3,5 +3,5 @@ from django.forms import Form, CharField, Textarea
 
 class UserPostForm(Form):
     text = CharField(widget=Textarea(
-        attrs={'cols': 100, 'rows': 5}),
-        label="Enter your post here")
+        attrs={'cols': 100, 'rows': 5, 'placeholder': "What's on your mind?"}),
+        label='')
